50bbddb0fe4c5cc0287611f0a1ca5831b2b37471
Jimersy Lee <jimersylee@gmail.com>
2017-06-17 09:01:37 +0800

    删除idea配置 增加获取服务时增加配置参数

--- Andromeda/Di/FactoryDefault.php
+++ Andromeda/Di/FactoryDefault.php
@@ -7,6 +7,7 @@
  */
 
 namespace Framework\Di\FactoryDefault;
+
 use Framework\Di;
 
 
@@ -22,33 +23,41 @@ class FactoryDefault extends Di
      * @param $name
      * @param $definition
      */
-    public function setShared($name,$definition){
-        $this->_sharedInstances[$name]=$definition;
+    public function setShared($name, $definition)
+    {
+        $this->_sharedInstances[$name] = $definition;
     }
 
 
     /**
      * 获取服务实例
-     * @param $name
+     * @param $name :服务名
+     * @param $param_arr :配置
      * @return mixed
+     * @throws \Exception
      */
-    public function get($name){
+    public function get($name, $param_arr = null)
+    {
 
-        $instance=null;
+        $instance = null;
         if (isset($this->_sharedInstances[$name])) {
             $definition = $this->_sharedInstances[$name];
         } else {
-            throw new \Exception("Service '" . name . "' wasn't found in the dependency injection container");
+            throw new \Exception("Service '" . $name . "' wasn't found in the dependency injection container");
         }
 
-        if (is_object($definition)) {
+        if (!is_object($definition)) {
+            throw new \Exception("Service $name wasn't a object");
+        }
+
+        if ($param_arr == null) {
             $instance = call_user_func($definition);
+        } else {
+            $instance = call_user_func_array($definition, $param_arr);
         }
-
         return $instance;
 
     }
-
 
 
     public function __construct()
--- App/Config/service.php
+++ App/Config/service.php
@@ -9,8 +9,9 @@
 //服务配置
 use Framework\Mvc\Writer;
 
-
-$di->setShared("writer",function() use ($di){
+$config=[];
+//闭包 use支持多多个参数,可以利用此特性传递多个参数
+$di->setShared("writer",function() use ($di,$config){
         $write=new Writer($di);
         $write->setDI($di);
         return $write;
